17b79c5cc6693c886a0e976e2c0424b7a49de622
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-09-08 22:54:38 -0600

    creacion del sistema de boost
    Contiene todo lo necesario para que el sistema de boost funcione correctamente

---
+++ src/systems/boosts/boost.js
@@ -0,0 +1,26 @@
+const { ChannelType } = require("discord.js");
+const { crearEmbed } = require('@/utils/embedUtil');
+const { BOOST_CHANNEL_ID } = require('@/utils/constantsUtil');
+
+module.exports = {
+    name: "guildMemberUpdate",
+    async execute(oldMember, newMember) {
+        const canal = newMember.guild.channels.cache.get(BOOST_CHANNEL_ID);
+        if (!canal || canal.type !== ChannelType.GuildText) return;
+
+        if (!oldMember.premiumSince && newMember.premiumSince) {
+            const embed = crearEmbed(
+                "Booster",
+                "¡Gracias por mejorar nuestro servidor!",
+                0xFF73FA 
+            );
+
+            embed.addFields({ name: "Booster", value: `${newMember}` });
+
+            canal.send({
+                content: `${newMember} acaba de mejorar el servidor.`,
+                embeds: [embed]
+            });
+        }
+    }
+};
