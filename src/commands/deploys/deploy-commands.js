8545e679d547e268d5c87b6acb5b661a6a31f686
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-12-23 13:33:55 -0600

    Creacion de sistema de encuestas
    Contiene toda la logica para el sistema de creacion de encuestas y el registro del comando de crear encuestas, el cual es (/encuestacrear)

---
+++ src/commands/deploys/deploy-commands.js
@@ -0,0 +1,31 @@
+require('module-alias/register');
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+const encuestaSystem = require('@/systems/encuestas/encuesta');
+
+const commands = [
+    encuestaSystem.data.toJSON()
+];
+
+const rest = new REST().setToken(process.env.BOT_TOKEN);
+
+(async () => {
+    try {
+        console.log(`🔄 Registrando ${commands.length} comando(s) slash...`);
+        if (process.env.GUILD_ID) {
+            const data = await rest.put(
+                Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+                { body: commands },
+            );
+            console.log(`✅ ${data.length} comando(s) registrado(s) en el servidor!`);
+        } else {
+            const data = await rest.put(
+                Routes.applicationCommands(process.env.CLIENT_ID),
+                { body: commands },
+            );
+            console.log(`✅ ${data.length} comando(s) registrado(s) globalmente!`);
+        }
+    } catch (error) {
+        console.error('❌ Error al registrar comandos:', error);
+    }
+})();
